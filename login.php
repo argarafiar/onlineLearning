c919562cd95f81fe5c2ad0fd6cbf286435fcce69
argarafiar <argarafiar@gmail.com>
2022-05-22 22:17:27 +0700

    membuat frontend dashboard

--- login.php
+++ login.php
@@ -24,7 +24,11 @@
                 //     setcookie("id", $row["id"], time() + 120);
                 //     setcookie("key", hash('sha256', $row["username"]), time() + 120);
                 // }
-                header("Location: index.php");
+                if($row["role"] == "admin"){
+                  header("Location: index.html?role=admin");
+                } else {
+                  header("Location: index.html?role=user");
+                }
                 exit;
             } else {
                 echo "<script>
@@ -45,7 +49,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     <title>Document</title>
 </head>
